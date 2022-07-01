d5bc8372e2b3d65aeccc0e05b68736e030943830
c-grattan <56635397+c-grattan@users.noreply.github.com>
2022-07-01 14:19:30 +0100

    Set up JUnit

--- vendingmachine/src/test/java/com/wiley/vendingmachine/service/VendingMachineServiceTest.java
+++ vendingmachine/src/test/java/com/wiley/vendingmachine/service/VendingMachineServiceTest.java
@@ -1,8 +1,8 @@
 package com.wiley.vendingmachine.service;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+
 public class VendingMachineServiceTest {
-	
-	public VendingMachineServiceTest() {
-		
-	}
 }
